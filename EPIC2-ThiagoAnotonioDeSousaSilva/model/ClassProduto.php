f1ae3da43ff26b8578a319ea348482b5a6a43c64
ThiagoSilva <thiagoantonio2108@gmail.com>
2024-11-30 16:23:10 -0300

    Adição de ClasseProduto

---
+++ EPIC2-ThiagoAnotonioDeSousaSilva/model/ClassProduto.php
@@ -0,0 +1,64 @@
+<?php
+
+class ClassEstoque
+{
+    private $idproduto;
+    private $imagem;
+    private $nome;
+    private $quantidade;
+    private $preco;
+
+    //GETTERS    
+    public function getIdproduto()
+    {
+        return $this->idproduto;
+    }
+
+    public function getImagem()
+    {
+        return $this->imagem;
+    }
+
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    public function getQuantidade()
+    {
+        return $this->quantidade;
+    }
+
+    public function getPreco()
+    {
+        return $this->preco;
+    }
+
+    //SETTERS
+
+    public function setIdproduto($idproduto)
+    {
+        $this->idproduto = $idproduto;
+    }
+
+
+    public function setImagem($imagem)
+    {
+        $this->imagem = $imagem;
+    }
+
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+
+    public function setQuantidade($quantidade)
+    {
+        $this->quantidade = $quantidade;
+    }
+
+    public function setPreco($preco)
+    {
+        $this->preco = $preco;
+    }
+}
